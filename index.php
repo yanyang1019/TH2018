59ca853f9a7a13c063c5132e744714314ac6867e
Yan <yan@Yans-MacBook-Pro.local>
2018-10-13 18:42:09 -0500

    Detail Change
    Change the html info

--- index.php
+++ index.php
@@ -83,34 +83,36 @@
 <!-- Team Container -->
 <div class="w3-container w3-padding-64 w3-center" id="team">
 <h2>OUR TEAM</h2>
-<p>Meet the team - our office rats:</p>
 
 <div class="w3-row"><br>
 
 <div class="w3-quarter">
-  <img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
-  <h3>Johnny Walker</h3>
-  <p>Web Designer</p>
+  <img src="/PIC/Bin.jpg" alt="Bin" style="width:50%" class="w3-rectangle w3-hover-opacity">
+  <h3>Youngbin Ha</h3>
+  <p>Senior/CS</p>
+  <p>yh9kb@mail.missouri.edu</p>
 </div>
 
 <div class="w3-quarter">
-  <img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
-  <h3>Rebecca Flex</h3>
-  <p>Support</p>
+  <img src="/PIC/Yan.jpg" alt="Yan" style="width:50%" class="w3-rectangle w3-hover-opacity">
+  <h3>Yan Yang</h3>
+  <p>Senior/IT</p>
+  <p>yyq34@mail.missouri.edu</p>
 </div>
 
 <div class="w3-quarter">
-  <img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
-  <h3>Jan Ringo</h3>
-  <p>Boss man</p>
+  <img src="/PIC/Eric.jpg" alt="Eric" style="width:50%" class="w3-rectangle w3-hover-opacity">
+  <h3>Eric Lin</h3>
+  <p>Sophomore/CS</p>
+  <p>kl2cb@mail.missouri.edu</p>
 </div>
 
 <div class="w3-quarter">
-  <img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
-  <h3>Kai Ringo</h3>
-  <p>Fixer</p>
+  <img src="/PIC/Tim.jpg" alt="Tim" style="width:50%" class="w3-rectangle w3-hover-opacity">
+  <h3>Tim Kwon</h3>
+  <p>Junior/CS</p>
+  <p>jk4g2@mail.missouri.edu</p>
 </div>
-
 </div>
 </div>
 
@@ -240,14 +242,14 @@
   <div class="w3-row">
     <div class="w3-col m5">
     <div class="w3-padding-16"><span class="w3-xlarge w3-border-teal w3-bottombar">Contact Us</span></div>
-      <h3>Address</h3>
-      <p>Swing by for a cup of coffee, or whatever.</p>
-      <p><i class="fa fa-map-marker w3-text-teal w3-xlarge"></i>  Chicago, US</p>
-      <p><i class="fa fa-phone w3-text-teal w3-xlarge"></i>  +00 1515151515</p>
-      <p><i class="fa fa-envelope-o w3-text-teal w3-xlarge"></i>  test@test.com</p>
+      <h3>Location</h3>
+      <p>230 Jesse Hall <br/> Columbia, MO.</p>
+      <p><i class="fa fa-map-marker w3-text-teal w3-xlarge"></i>  Columbia, US</p>
+      <p><i class="fa fa-phone w3-text-teal w3-xlarge"></i>  217-381-6949</p>
+      <p><i class="fa fa-envelope-o w3-text-teal w3-xlarge"></i>  kl2cb@mail.missouri.edu</p>
     </div>
     <div class="w3-col m7">
-      <form class="w3-container w3-card-4 w3-padding-16 w3-white" action="/action_page.php" target="_blank">
+      <form class="w3-container w3-card-4 w3-padding-16 w3-white" method = "post" action="email_me.php">
       <div class="w3-section">      
         <label>Name</label>
         <input class="w3-input" type="text" name="Name" required>
